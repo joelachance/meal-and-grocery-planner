3c42398c2f528f1992ab369999c9b45a98ecb59e
Katya Weicht <katya.weicht@gmail.com>
2025-08-25 12:20:38 -0600

    Create user schema with validations

--- server/models.py
+++ server/models.py
@@ -1,18 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.hybrid import hybrid_property
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, ValidationError, validate
 from app import bcrypt
+import re
 
 db = SQLAlchemy()
+password_regex = re.compile('^(?=\S{8,20}$)(?=.*?\d)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[^A-Za-z\s0-9])')
 
 class User(db.Model):
   __tablename__ = 'users'
 
   id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String)
+  name = db.Column(db.String, nullable=False)
   username = db.Column(db.String, unique=True, nullable=False)
-  _password_hash = db.Column(db.String)
+  _password_hash = db.Column(db.String, nullable=False)
 
   @hybrid_property
   def password_hash(self):
@@ -25,3 +27,19 @@ class User(db.Model):
 
   def authenticate(self,password):
     return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+  
+class UserSchema(Schema):
+  id = fields.Integer(dump_only=True)
+  name = fields.String(required = True, validate=validate.Length(min=1, max=50, error="name must be between 1 and 50 characters"))
+  username = fields.String(required=True)
+  password = fields.String(required=True, load_only=True)
+
+  @validates('username')
+  def validate_username(self,value, **kwargs):
+    if User.query.filter_by(username=value).first():
+      raise ValidationError("Username already exists, Please choose a different one")
+    
+  @validates('password')
+  def validate_password(self,value, **kwargs):
+    if not password_regex.match(value):
+      raise ValidationError("Password must be 8-20 characters long, contain at least one digit, one uppercase letter, one lowercase letter, and one special symbol.")
