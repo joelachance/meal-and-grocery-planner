3a5dfa24417ad4513b5cd736d2e3f860efbe65c9
Katya Weicht <katya.weicht@gmail.com>
2025-08-27 15:03:48 -0600

    In login component render either login or signup form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,26 @@
 import React from "react"
+import LoginForm from '../components/LoginForm' 
+import SignupForm from '../components/SignupForm' 
 
-function Login() {
+function Login({onLogin}) {
+  const [showLogin, setShowLogin] = useState(true);
   return(
-    <>
-    </>
-  )
+    showLogin ? (
+      <>
+        <LoginForm onLogin={onLogin}/>
+        <p>Dont have an account?
+          <Button onClick={() => setShowLogin(false)}>Sign Up</Button>
+        </p>
+      </>
+    ) : (
+      <>
+        <SignupForm onLogin={onLogin} />
+        <p>Already have an account?
+          <Button onClick={() => setShowLogin(true)}>Log In</Button>
+        </p>
+      </>
+    )
+    ) 
 }
 
 export default Login
