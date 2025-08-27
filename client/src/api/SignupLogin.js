bbb76a0db05a035387cbee150d937a2b8e502dd8
Katya Weicht <katya.weicht@gmail.com>
2025-08-27 14:22:58 -0600

    Create function for login

--- client/src/api/SignupLogin.js
+++ client/src/api/SignupLogin.js
@@ -15,3 +15,21 @@ export async function signup(username, password, passwordConfirmation) {
     return null
   }
 }
+
+export async function login(username, password) {
+  try {
+    const response = await fetch('http://127.0.0.1:5555/login', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({username, password})
+    })
+    const data = await response.json()
+    localStorage.setItem("token", data.token)
+    return data
+  } catch (error) {
+    console.error("Error logging in:", error)
+    return null
+  }
+}
