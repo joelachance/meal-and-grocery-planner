01cab089ba0489e43f08209597a50cd0e0df7203
Katya Weicht <katya.weicht@gmail.com>
2025-08-27 21:55:25 -0600

    Create proxy for routes

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,24 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5555',
+        changeOrigin: true
+      },
+      '/login': {
+        target: 'http://127.0.0.1:5555',
+        changeOrigin: true
+      },
+      '/signup': {
+        target: 'http://127.0.0.1:5555',
+        changeOrigin: true
+      },
+      '/me': {
+        target: 'http://127.0.0.1:5555',
+        changeOrigin: true
+      }
+    }
+  }
 })
