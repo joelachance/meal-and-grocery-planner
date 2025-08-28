51fcada129027b5d892767c9cab7c101e8153f8f
Katya Weicht <katya.weicht@gmail.com>
2025-08-28 13:12:17 -0600

    remove token from local storage and set user state to null upon clicking logout button

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,13 +1,20 @@
 import React from "react"
 import {NavLink} from "react-router-dom"
+import {useContext} from "react"
+import {UserContext} from '../UserContext'
 
 function NavBar() {
+  const setUser = useContext(UserContext)
+  function handleLogout() {
+    localStorage.removeItem('token')
+    setUser(null)
+  }
   return (
     <nav className='navbar'>
       <NavLink to="/" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Home</NavLink>
       <NavLink to="/recipes" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Recipes</NavLink>
       <NavLink to="/Grocery" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Grocery</NavLink>
-      <button>Logout</button>
+      <button onClick={handleLogout}>Logout</button>
     </nav>
   )
 }
