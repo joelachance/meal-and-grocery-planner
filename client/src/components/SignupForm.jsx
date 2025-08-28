f89b02741b5f649039a87fc80f3b9951bfd0989c
Katya Weicht <katya.weicht@gmail.com>
2025-08-28 00:17:01 -0600

    Create and style signup form

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,7 +1,28 @@
-function SignupForm() {
+function SignupForm({onLogin}) {
   return (
-    <div>
-
+    <div className="signup-form-div">
+      <form className='signup-form'>
+        <h1>Create an Account</h1>
+        <div>
+          <label htmlFor='name'>Name:</label>
+          <input type='text' id='name'/>
+        </div>
+        <div>
+          <label htmlFor='username'>Username:</label>
+          <input type='text' id='username' />
+        </div>
+        <div>
+          <label htmlFor='password'>Password:</label>
+          <input type='text' id='password' />
+        </div>
+        <div>
+          <label htmlFor='confirmPassword'>Confirm password:</label>
+          <input type='text' id='confirmPassword' />
+        </div>
+        <div className="signup-button-div">
+        <button className="signup-button" type='submit' >Login</button>
+        </div>
+      </form>
     </div>
   )
 }
