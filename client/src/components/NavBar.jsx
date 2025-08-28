4e7b0427f85fb7ade92f4ee251502d9661ba7198
Katya Weicht <katya.weicht@gmail.com>
2025-08-27 23:28:47 -0600

    Create navbar

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import {NavLink} from "react-router-dom"
+
+function NavBar() {
+  return (
+    <nav className='navbar'>
+      <NavLink to="/" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Home</NavLink>
+      <NavLink to="/recipes" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Recipes</NavLink>
+      <NavLink to="/Grocery" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Grocery</NavLink>
+      <button onClick={handleLogout}>Logout</button>
+    </nav>
+  )
+}
+
+export default NavBar
