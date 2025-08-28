ac31db42d9638416ee23367b6d8e468439f8ee1f
Katya Weicht <katya.weicht@gmail.com>
2025-08-28 17:02:37 -0600

    Style navbar and import into navbar component

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import {NavLink} from "react-router-dom"
 import {useContext} from "react"
 import {UserContext} from '../UserContext'
+import '../styles/navbar.css'
 
 function NavBar() {
   const setUser = useContext(UserContext)
@@ -11,10 +12,12 @@ function NavBar() {
   }
   return (
     <nav className='navbar'>
-      <NavLink to="/" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Home</NavLink>
-      <NavLink to="/recipes" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Recipes</NavLink>
-      <NavLink to="/Grocery" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Grocery</NavLink>
-      <button onClick={handleLogout}>Logout</button>
+      <div className='nav-links'>
+        <NavLink to="/" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Home</NavLink>
+        <NavLink to="/recipes" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Recipes</NavLink>
+        <NavLink to="/Grocery" className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Grocery</NavLink>
+      </div>
+      <button onClick={handleLogout} className='logout-button'>Logout</button>
     </nav>
   )
 }
