a9bc109a6f03bc4fa770b967ff9ee440580c5345
Katya Weicht <katya.weicht@gmail.com>
2025-08-29 14:15:56 -0600

    Navigate to add recipe form on button click

--- client/src/pages/Recipes.jsx
+++ client/src/pages/Recipes.jsx
@@ -2,20 +2,31 @@ import React from "react"
 import {useState} from "react"
 import NavBar from "../components/NavBar"
 import '../styles/recipePage.css'
+import {useNavigate} from "react-router-dom"
 
 import RecipesByCuisine from "../components/RecipesByCuisine"
 
 function Recipes() {
   const [cuisine, setCuisine] = useState("")
+  const navigate = useNavigate()
+
   async function handleFetchRecipes(event) {
     const {value} = event.target
     setCuisine(value)
   }
+
+  function handleCreateRecipe() {
+    navigate('/recipes//addRecipes')
+  }
+
   return(
     <div>
       <NavBar />
       <div className="recipes-content">
-        <h1>Browse recipes</h1>
+        <div>
+          <h1>Browse recipes</h1>
+          <button onClick={handleCreateRecipe}>Add your own recipe</button>
+        </div>
         <div className='cuisine-buttons'>
           <button onClick={handleFetchRecipes} value={'italian'}>Italian</button>
           <button onClick={handleFetchRecipes} value={'greek'}>Greek</button>
