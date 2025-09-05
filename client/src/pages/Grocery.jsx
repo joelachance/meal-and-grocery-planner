b6f448f311cb407574feb93f803dee1176bd5095
Katya Weicht <katya.weicht@gmail.com>
2025-09-05 12:29:38 -0600

    Add error handling to grocery form

--- client/src/pages/Grocery.jsx
+++ client/src/pages/Grocery.jsx
@@ -7,8 +7,11 @@ import '../styles/grocery.css'
 
 function Grocery() {
   const { user } = useContext(UserContext)
-
   const [recipes, setRecipes] = useState([])
+  const [dates, setDates] = useState({ start: "", end: "" })
+  const [filteredRecipes, setFilteredRecipes] = useState([])
+  const [recipeStatus, setRecipeStatus] = useState(true)
+  const [errorMessage, setErrorMessage] = useState("")
 
   useEffect(() => {
     if(user) {
@@ -16,11 +19,31 @@ function Grocery() {
     } 
   },[user])
 
-  function handleSubmit(event) {
-    event.preventDefault()
+  function handleChange(event) {
+    const {name, value} = event.target
+    setDates(prev => ({ 
+      ...prev, 
+      [name]: value }))
   }
 
-  console.log(recipes)
+  function handleSubmit(event) {
+    event.preventDefault()
+    if(!dates.start || !dates.end) {
+      setErrorMessage('Error, please input a start and end date')
+      return
+    }
+    setErrorMessage("")
+    setRecipeStatus(true)
+    const recipeData = recipes.filter((recipe) => {
+      return recipe.date >= dates.start && recipe.date <= dates.end
+    })
+    setFilteredRecipes(recipeData)
+    if(recipeData.length === 0) {
+      setRecipeStatus(false)
+    }
+  }
+
+  
   return(
     <>
       <NavBar />
@@ -28,11 +51,16 @@ function Grocery() {
         <h2>Generate a Grocery List</h2>
         <form onSubmit={handleSubmit} className='grocery-form'>
           <label htmlFor='start'>Start Date:</label>
-          <input type='date' id='start' name='start'/>
+          <input type='date' id='start' name='start'value={dates.start} onChange={handleChange}/>
           <label htmlFor='end'>End Date:</label>
-          <input type='date' id='end' name='end' />
+          <input type='date' id='end' name='end' value={dates.end} onChange={handleChange}/>
           <button type='submit'>Submit</button>
         </form>
+        {errorMessage && <p>{errorMessage}</p>}
+        {recipeStatus === false && !errorMessage ?
+          <p>No recipes found for these dates</p> :
+          <div> </div>
+        }
       </div>
     </>
   )
