2ea97cfa51275dafc498d2877b6bbecb25580595
Katya Weicht <katya.weicht@gmail.com>
2025-09-05 11:56:59 -0600

    Create grocery list form and add initial styling

--- client/src/pages/Grocery.jsx
+++ client/src/pages/Grocery.jsx
@@ -1,11 +1,39 @@
 import React from "react"
 import NavBar from "../components/NavBar"
+import { useState, useEffect } from "react"
+import {useContext} from "react"
+import {UserContext} from '../UserContext'
+import '../styles/grocery.css'
 
 function Grocery() {
+  const { user } = useContext(UserContext)
+
+  const [recipes, setRecipes] = useState([])
+
+  useEffect(() => {
+    if(user) {
+      setRecipes(user.recipes)
+    } 
+  },[user])
+
+  function handleSubmit(event) {
+    event.preventDefault()
+  }
+
+  console.log(recipes)
   return(
     <>
       <NavBar />
-      <p>On this page you can view your calendar by week and generate a grocery list for a particular week</p>
+      <div className='grocery-div'>
+        <h2>Generate a Grocery List</h2>
+        <form onSubmit={handleSubmit} className='grocery-form'>
+          <label htmlFor='start'>Start Date:</label>
+          <input type='date' id='start' name='start'/>
+          <label htmlFor='end'>End Date:</label>
+          <input type='date' id='end' name='end' />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
     </>
   )
 }
