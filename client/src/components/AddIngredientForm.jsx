a684e9f9953b4796203af711eff9cf676796509b
Katya Weicht <katya.weicht@gmail.com>
2025-09-06 16:39:39 -0600

    Add error handling to add ingredient form

--- client/src/components/AddIngredientForm.jsx
+++ client/src/components/AddIngredientForm.jsx
@@ -18,9 +18,24 @@ function AddIngredientForm({recipe_id}) {
 
   async function handleSubmit(event) {
     event.preventDefault()
+    //give error messages if any inputs are blank
+    let newErrors = {}
+    if (!newIngredient.name || newIngredient.name.trim() === "") {
+      newErrors.name = 'Name cannot be empty'
+    }
+    if (!newIngredient.quantity || newIngredient.quantity === "") {
+      newErrors.quantity = 'quantity cannot be empty'
+    }
+    if (!newIngredient.quantity_description || newIngredient.quantity_description.trim() === "") {
+      newErrors.quantity_description = 'quantity_description cannot be empty'
+    }
+    setErrors(newErrors)
+    //dont even try the post request if there are errors
+    if (Object.keys(newErrors).length > 0) return
 
     const result = await addIngredient(newIngredient, recipe_id)
     if (!result.error) {
+      setErrors({})
       alert('Ingredient(s) successfully added!')
       const addedIngredient = result
       setUser(prev => ({
@@ -38,13 +53,25 @@ function AddIngredientForm({recipe_id}) {
   return (
     <div className='add-ingredient-form-div'>
       <form className='add-ingredient-form' onSubmit={handleSubmit}>
-        <label htmlFor='name'>Name:</label>
-        <input id='name' name='name' type='text' value={newIngredient.name} onChange={handleChange}/>
-        <label htmlFor='quantity'>Quantity:</label>
-        <input id='quantity' name='quantity' type='number' value={newIngredient.quantity} onChange={handleChange}/>
-        <label htmlFor='quantity_description'>Quantity description (oz, cups, lbs, ect.):</label>
-        <input id='quantity_description' name='quantity_description' type='text' value={newIngredient.quantity_description} onChange={handleChange}/>
-        <button type='submit'>Submit</button>
+        <div className='form-row'>
+          <label htmlFor='name'>Name:</label>
+          <input id='name' name='name' type='text' value={newIngredient.name} onChange={handleChange}/>
+          {/* {errors?.name && <p>{errors.name}</p>} */}
+          <label htmlFor='quantity'>Quantity:</label>
+          <input id='quantity' name='quantity' type='number' min="1" value={newIngredient.quantity} onChange={handleChange}/>
+          {/* {errors?.quantity && <p>{errors.quantity}</p>} */}
+          <label htmlFor='quantity_description'>Quantity description (oz, cups, lbs, ect.):</label>
+          <input id='quantity_description' name='quantity_description' type='text' value={newIngredient.quantity_description} onChange={handleChange}/>
+          {/* {errors?.quantity_description && <p>{errors.quantity_description}</p>} */}
+          <button type='submit'>Submit</button>
+        </div>
+        {errors &&
+          <div className='error-div'>
+            {errors.name && <p>{errors.name}</p>}
+            {errors.quantity && <p>{errors.quantity}</p>}
+            {errors.quantity_description && <p>{errors.quantity_description}</p>}
+          </div>
+        }
       </form>
     </div>
   )
